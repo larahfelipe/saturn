cc561a9073180fd5bc833ee028124147c40d10af
Felipe Lara <68015575+felpshn@users.noreply.github.com>
2020-10-26 18:54:39 -0300

    some updts

---
+++ README.md
@@ -0,0 +1,39 @@
+<p align="center">
+    <a href="https://github.com/felpshn/unity-bot">
+       <img width="128" height="128" src="https://github.com/felpshn/unity-bot/blob/master/.github/unity-logo.png"> 
+    </a>
+</p>
+
+<p align="center">
+    <a href="https://github.com/felpshn/unity-bot">
+        <img src="https://img.shields.io/badge/version-1.0-lightgrey">
+    </a>
+    <a href="https://nodejs.org/en/">
+        <img src="https://img.shields.io/badge/built%20with-Node%20JS-green">
+    </a>
+    <a href="https://github.com/discordjs/discord.js/">
+        <img src="https://img.shields.io/badge/built%20with-Discord%20JS-blue">
+    </a>
+    <a href="https://github.com/felpshn/unity-bot/blob/master/LICENSE">
+        <img src="https://img.shields.io/badge/license-MIT-orange">
+    </a>
+</p>
+
+## :scroll: About
+
+A discord bot built with JavaScript, NodeJS and DiscordJS. 
+
+## :question: How to use
+
+**1)** Clone this repo:
+```elm
+git clone https://github.com/felpshn/unity-bot.git
+
+cd unity-bot
+```
+**2)** Setting up and running:
+```elm
+npm install
+
+npm run dev
+```
---
+++ src/index.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+const embed = new Discord.MessageEmbed();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const fs = require('fs');
+const path = require('path');
+
+const commandsDir = fs.readdirSync(path.join(__dirname, '/commands'));
+commandsDir.filter(file => {
+    file.endsWith('.js');
+});
+for (let file of commandsDir) {
+    const handleFile = require(`./commands/${file}`);
+    bot.commands.set(handleFile.name, handleFile);
+};
+
+
+bot.once('ready', () => {
+    console.log('\n[unity] Okay, I\'m listening ...\n');
+});
+
+bot.on('message', async msg => {
+    bot.user.setActivity('.help');
+
+    const prefix = process.env.BOT_PREFIX; 
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+    const args = msg.content.slice(prefix.length).trim().split(/ +/);
+    const commandListener = args.shift().toLowerCase();
+    console.log(`[@${msg.author.tag}] >> ${prefix + commandListener} ${args.join(' ')}`);
+
+    try {
+        bot.commands.get(commandListener).execute(bot, msg, args);
+    } catch (e) {
+        console.log(e);
+        embed
+        .setTitle('Error 404: Command not found!')
+        .setDescription('If you need help with commands type **\`.help\`**')
+        .setColor('#C1FF00');
+        msg.channel.send({ embed });
+    };
+});
+
+bot.login(process.env.BOT_TOKEN);
