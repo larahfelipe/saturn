7a2f069af1fb12a37580e7b925b694db96935d1c
Felipe Lara <68015575+felpshn@users.noreply.github.com>
2021-04-17 20:37:16 -0300

    Update: Play music function

--- dist/commands/user/music/Play.js
+++ dist/commands/user/music/Play.js
@@ -8,35 +8,88 @@ const axios_1 = __importDefault(require("axios"));
 const yt_search_1 = __importDefault(require("yt-search"));
 const ytdl_core_1 = __importDefault(require("ytdl-core"));
 const discord_js_1 = require("discord.js");
+const FormatSecondsToTime_1 = require("../../../utils/FormatSecondsToTime");
 const ReactionsHandler_1 = require("../../../utils/ReactionsHandler");
 const DropBotQueueConnection_1 = require("../../../utils/DropBotQueueConnection");
 async function run(bot, msg, args) {
-    let querySong = args.join(' ');
+    if (!args)
+        return msg.reply('You need to give me a song to play it!');
+    let requestedSong = args.join(' ');
+    let spotifyPlaylistTracks = [];
+    let spotifyPlaylistDuration = 0;
     let song;
     try {
-        if (ytdl_core_1.default.validateURL(querySong)) {
-            song = await yt_search_1.default({ videoId: ytdl_core_1.default.getURLVideoID(querySong) });
-            handlePlaySong();
+        if (ytdl_core_1.default.validateURL(requestedSong)) {
+            song = await yt_search_1.default({ videoId: ytdl_core_1.default.getURLVideoID(requestedSong) });
+            return handlePlaySong(true);
+        }
+        else if (requestedSong.startsWith('https://open.spotify.com/')) {
+            await axios_1.default
+                .get(requestedSong)
+                .then(({ data }) => {
+                let contextSelector;
+                if (requestedSong.charAt(25) === 't') {
+                    contextSelector = data.substring(data.indexOf('<ti') + 7, data.indexOf('|') - 1);
+                    requestedSong = contextSelector;
+                }
+                else if (requestedSong.charAt(25) === 'p') {
+                    contextSelector = data.substring(data.indexOf('Spotify.Entity') + 17, data.indexOf('"available_markets"') - 1) + '}';
+                    const spotifyPlaylist = JSON.parse(contextSelector);
+                    spotifyPlaylistTracks = spotifyPlaylist.tracks.items.map(song => {
+                        spotifyPlaylistDuration += song.track.duration_ms;
+                        return `${song.track.name} - ${song.track.album.artists[0].name}`;
+                    });
+                    const embed = new discord_js_1.MessageEmbed();
+                    embed
+                        .setAuthor(`"${spotifyPlaylist.name}"\nSpotify playlist by ${spotifyPlaylist.owner.display_name}`)
+                        .setDescription(`\n• Total playlist tracks: \`${spotifyPlaylist.tracks.items.length}\`\n• Playlist duration: \`${FormatSecondsToTime_1.formatSecondsToTime(spotifyPlaylistDuration / 1000)}\``)
+                        .setThumbnail(spotifyPlaylist.images[0].url)
+                        .setTimestamp(Date.now())
+                        .setFooter('Spotify | Music for everyone')
+                        .setColor('#6E76E5');
+                    msg.channel.send({ embed });
+                }
+                else
+                    throw new Error('Invalid URL');
+            })
+                .catch((err) => console.error(err));
+        }
+        if (spotifyPlaylistTracks.length > 0) {
+            const playlistTracks = [];
+            spotifyPlaylistTracks.forEach(track => {
+                yt_search_1.default(track, (err, res) => {
+                    if (err)
+                        throw err;
+                    if (res && res.videos.length > 0) {
+                        playlistTracks.push(res.videos[0]);
+                    }
+                });
+            });
+            const embed = new discord_js_1.MessageEmbed();
+            embed
+                .setTitle('Gotcha!, loading playlist songs ... ⏳')
+                .setDescription('I\'ll join the party in 1 minute, please wait')
+                .setColor('#6E76E5');
+            msg.channel.send({ embed });
+            setTimeout(() => {
+                song = playlistTracks[0];
+                handlePlaySong(false);
+                playlistTracks.shift();
+                playlistTracks.forEach(track => {
+                    setTimeout(() => {
+                        song = track;
+                        handlePlaySong(false);
+                    }, 5000);
+                });
+            }, 60000);
         }
         else {
-            if (querySong.startsWith('https://open.spotify.com/track/')) {
-                await axios_1.default
-                    .get(querySong)
-                    .then(({ data }) => {
-                    const htmlData = data;
-                    querySong = htmlData.substring(htmlData.indexOf('<ti') + 7, htmlData.indexOf('|') - 1);
-                })
-                    .catch((err) => {
-                    console.log('Error search');
-                    throw err;
-                });
-            }
-            yt_search_1.default(querySong, (err, res) => {
+            yt_search_1.default(requestedSong, (err, res) => {
                 if (err)
-                    return console.error(err);
+                    throw err;
                 if (res && res.videos.length > 0) {
                     song = res.videos[0];
-                    handlePlaySong();
+                    handlePlaySong(true);
                 }
                 else
                     return msg.reply('Sorry!, I couldn\'t find any song related to your search.');
@@ -45,11 +98,10 @@ async function run(bot, msg, args) {
     }
     catch (err) {
         console.error(err);
-        msg.reply('You need to give me a song in order to play it!');
     }
-    function handlePlaySong() {
-        const queueExists = bot.queues.get(msg.guild.id);
-        if (!queueExists) {
+    function handlePlaySong(sendQueueNotifMsg) {
+        const queue = bot.queues.get(msg.guild.id);
+        if (!queue) {
             setSong(bot, msg, song, msg.author.id);
             const embed = new discord_js_1.MessageEmbed();
             embed
@@ -59,17 +111,19 @@ async function run(bot, msg, args) {
             msg.channel.send({ embed });
         }
         else {
-            queueExists.songs.push(song);
-            queueExists.authors.push(msg.author.id);
-            bot.queues.set(msg.guild.id, queueExists);
-            const embed = new discord_js_1.MessageEmbed();
-            embed
-                .setTitle('📃  Queue')
-                .setDescription(`Got it! [${song.title}](${song.url}) was added to the queue and his current position is \`${queueExists.songs.indexOf(song)}\`.\n\nYou can see the guild's queue anytime using \`.queue\``)
-                .setFooter(`Added by ${msg.author.username}`, msg.author.displayAvatarURL())
-                .setTimestamp(new Date())
-                .setColor('#6E76E5');
-            msg.channel.send({ embed });
+            queue.songs.push(song);
+            queue.authors.push(msg.author.id);
+            bot.queues.set(msg.guild.id, queue);
+            if (sendQueueNotifMsg) {
+                const embed = new discord_js_1.MessageEmbed();
+                embed
+                    .setTitle('📃  Queue')
+                    .setDescription(`Got it! [${song.title}](${song.url}) was added to the queue and his current position is \`${queue.songs.indexOf(song)}\`.\n\nYou can see the guild's queue anytime using \`.queue\``)
+                    .setFooter(`Added by ${msg.author.username}`, msg.author.displayAvatarURL())
+                    .setTimestamp(new Date())
+                    .setColor('#6E76E5');
+                msg.channel.send({ embed });
+            }
         }
     }
 }
@@ -83,7 +137,7 @@ async function setSong(bot, msg, song, msgAuthor) {
         }
     }
     if (!((_a = msg.member) === null || _a === void 0 ? void 0 : _a.voice.channel))
-        return msg.reply('You need to be in a voice channel in order to play a song!');
+        return msg.reply('You need to be in a voice channel to play a song!');
     if (!queue) {
         const botConnection = await msg.member.voice.channel.join();
         queue = {
