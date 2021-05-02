8d25735179c9223280264f7bf4b67cf82a26cf0f
Felipe Lara <68015575+felpshn@users.noreply.github.com>
2021-05-02 13:33:00 -0300

    Feat: Developer's database management functions added

--- dist/services/UpdateMemberService.js
+++ dist/services/UpdateMemberService.js
@@ -1,13 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handleMemberDemotion = exports.handleMemberElevation = void 0;
+const ParseMembers_1 = require("../utils/ParseMembers");
 const Member_1 = require("../models/Member");
 async function handleMemberElevation(member) {
-    const memberExists = await Member_1.Member.findOne({ userID: member.id });
+    const [memberExists, memberId] = await ParseMembers_1.parseMember(member);
     if (!memberExists)
         throw new Error();
     await Member_1.Member.findOneAndUpdate({
-        userID: member.id
+        userID: memberId
     }, {
         $set: {
             roleLvl: 1
@@ -16,11 +17,11 @@ async function handleMemberElevation(member) {
 }
 exports.handleMemberElevation = handleMemberElevation;
 async function handleMemberDemotion(member) {
-    const memberExists = await Member_1.Member.findOne({ userID: member.id });
+    const [memberExists, memberId] = await ParseMembers_1.parseMember(member);
     if (!memberExists)
         throw new Error();
     await Member_1.Member.findOneAndUpdate({
-        userID: member.id
+        userID: memberId
     }, {
         $set: {
             roleLvl: 0
