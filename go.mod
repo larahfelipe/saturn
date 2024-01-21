2a2c6a42e22b305b3f80135f4ad614e5b3475be0
Felipe Lara <felipeahlara@gmail.com>
2024-01-20 21:31:14 -0300

    Feat: Audio streaming implementation and more improvements

--- go.mod
+++ go.mod
@@ -4,11 +4,22 @@ go 1.21.3
 
 require github.com/bwmarrin/discordgo v0.27.1
 
-require github.com/jonas747/ogg v0.0.0-20161220051205-b4f6f4cf3757 // indirect
+require (
+	github.com/bitly/go-simplejson v0.5.1 // indirect
+	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/dop251/goja v0.0.0-20231027120936-b396bb4c349d // indirect
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
+	github.com/google/pprof v0.0.0-20231101202521-4ca4178f5c7a // indirect
+	github.com/jonas747/dca v0.0.0-20210930103944-155f5e5f0cc7 // indirect
+	github.com/jonas747/ogg v0.0.0-20161220051205-b4f6f4cf3757 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
 
 require (
-	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/jonas747/dca v0.0.0-20210930103944-155f5e5f0cc7
-	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/kkdai/youtube/v2 v2.10.0
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,46 +1,212 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"os/signal"
+	"reflect"
+	"regexp"
 	"strings"
+	"sync"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	env "github.com/joho/godotenv"
 	"github.com/jonas747/dca"
+	"github.com/kkdai/youtube/v2"
 )
+
+type Stream struct {
+	Url          string
+	MimeType     string
+	AudioQuality string
+	Bitrate      int
+	Readable     io.ReadCloser
+}
 
 type Song struct {
 	Title       string
 	Url         string
-	StreamUrl   string
-	Duration    string
 	ArtworkUrl  string
+	Duration    float64
 	RequestedBy string
 	Position    int
+	Stream      *Stream
 }
 
 type Queue struct {
-	Lock  bool
-	Songs []Song
+	Status string
+	Mutex  *sync.Mutex
+	Songs  []Song
 }
 
-type Bot struct {
-	Token         string
-	CommandPrefix string
-	Session       *discordgo.Session
-	Queue         *Queue
+type Integrations struct {
+	Youtube *youtube.Client
 }
 
-type Command struct {
+type Features struct {
+	Integrations *Integrations
+	Queue        *Queue
+}
+
+type CallableCommand struct {
 	Name    string
-	Args    []string
 	Execute func(bot *Bot, session *discordgo.Session, message *discordgo.MessageCreate, args []string)
 }
 
-func (queue *Queue) add(song Song) {
-	queue.Songs = append(queue.Songs, song)
+type Command struct {
+	Prefix string
+	All    []CallableCommand
+}
+
+type Bot struct {
+	Token    string
+	Session  *discordgo.Session
+	Command  *Command
+	Features *Features
+}
+
+const (
+	Idle    = "IDLE"
+	Playing = "PLAYING"
+	Paused  = "PAUSED"
+)
+
+func printObject(obj interface{}) {
+	valueOfObj := reflect.ValueOf(obj)
+
+	if valueOfObj.Kind() != reflect.Struct {
+		log.Println("input object is not a struct")
+		return
+	}
+
+	for i := 0; i < valueOfObj.NumField(); i++ {
+		field := valueOfObj.Type().Field(i)
+		fieldValue := valueOfObj.Field(i).Interface()
+
+		fmt.Printf("%s: %v\n", field.Name, fieldValue)
+	}
+}
+
+func getFileExtFromMime(mimeType string) string {
+	pattern := `\/([^;\s]+)`
+	re := regexp.MustCompile(pattern)
+
+	sm := re.FindStringSubmatch(mimeType)
+	if len(sm) < 1 {
+		return ""
+	}
+
+	return sm[1]
+}
+
+func writeFile(readable io.ReadCloser, fileName string) error {
+	// TODO: Check if a file with the same name already exists and if so, rename it
+
+	f, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	if _, err := io.Copy(f, readable); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (song *Song) download() (string, error) {
+	fileExt := getFileExtFromMime(song.Stream.MimeType)
+	if len(fileExt) == 0 {
+		return "", errors.New("unable to determine file extension from mime type")
+	}
+
+	tempDirName := "temp"
+	if _, err := os.Stat(tempDirName); os.IsNotExist(err) {
+		if err := os.Mkdir(tempDirName, 0755); err != nil {
+			return "", err
+		}
+	}
+
+	fileName := fmt.Sprintf("%s.%s", song.Title, fileExt)
+	filePath := fmt.Sprintf("%s/%s", tempDirName, fileName)
+	if err := writeFile(song.Stream.Readable, filePath); err != nil {
+		return "", err
+	}
+
+	return filePath, nil
+}
+
+func (song *Song) stream(s *discordgo.Session, m *discordgo.MessageCreate) {
+	defer song.Stream.Readable.Close()
+
+	for _, guild := range s.State.Guilds {
+		for _, voiceState := range guild.VoiceStates {
+			if voiceState.UserID == m.Author.ID {
+				// TODO: check if the bot is already in the same voice channel as the user who requested the song
+
+				if voiceState.UserID != s.State.User.ID {
+					s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Yay! Joining the party on <#%s>", voiceState.ChannelID))
+				}
+
+				vc, err := s.ChannelVoiceJoin(guild.ID, voiceState.ChannelID, false, true)
+				if err != nil {
+					s.ChannelMessageSend(m.ChannelID, "Oops! It seems that I'm not in the mood for singing right now. Please try again later.")
+					log.Println("[exception] Voice connection join event error:", err)
+					return
+				}
+
+				if err := vc.Speaking(true); err != nil {
+					log.Println("[exception] Voice connection speak event error:", err)
+					return
+				}
+
+				defer vc.Speaking(false)
+
+				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Now playing [%s](%s)", song.Title, song.Url))
+
+				// NOTE: temporarily downloading the song to prevent unexpected streaming behavior.
+				// It appears that dca is either unable to encode the stream URL properly or the stream URL provided by youtube-dl is not fully compatible with dca.
+				// This is causing playback to stop before the song actually ends.
+
+				sfp, err := song.download()
+				if err != nil {
+					log.Println("[exception] Stream download error:", err)
+					return
+				}
+
+				defer os.Remove(sfp)
+
+				// TODO: check the bitrate of the voice channel and set the bitrate of the song accordingly
+
+				options := dca.StdEncodeOptions
+				options.RawOutput = true
+				options.Bitrate = 96
+
+				es, err := dca.EncodeFile(sfp, options)
+				if err != nil {
+					log.Println("[exception] Stream encode error:", err)
+					return
+				}
+
+				defer es.Cleanup()
+
+				done := make(chan error)
+				dca.NewStream(es, vc, done)
+
+				se := <-done
+				if se != nil && se != io.EOF {
+					log.Println("[exception] Stream broadcast error:", se)
+					return
+				}
+			}
+		}
+	}
 }
 
 func (queue *Queue) shift() Song {
@@ -54,106 +220,75 @@ func (queue *Queue) shift() Song {
 	return s
 }
 
-func (song *Song) streamNow(vc *discordgo.VoiceConnection, filePath string) {
-	options := dca.StdEncodeOptions
-	options.RawOutput = true
-	options.Bitrate = 96
+func (queue *Queue) add(song *Song) Song {
+	queue.Songs = append(queue.Songs, *song)
 
-	// TODO: fix the encoding and streaming process
-
-	es, err := dca.EncodeFile(filePath, options)
-	if err != nil {
-		fmt.Println("[exception] File encoding error:", err)
-		return
-	}
-
-	defer es.Cleanup()
-
-	if !vc.Ready {
-		fmt.Println("[exception] Voice connection is not ready")
-		return
-	}
-
-	vc.Speaking(true)
-	defer vc.Speaking(false)
-
-	done := make(chan error)
-	dca.NewStream(es, vc, done)
-
-	<-done
+	return *song
 }
 
 func (queue *Queue) process(s *discordgo.Session, m *discordgo.MessageCreate) {
+	queue.Mutex.Lock()
+	defer queue.Mutex.Unlock()
+
 	if len(queue.Songs) == 0 {
-		s.ChannelMessageSend(m.ChannelID, "Party's over. Goodbye!")
+		queue.Status = Idle
 		return
 	}
 
-	if queue.Lock {
-		return
-	}
+	queue.Status = Playing
 
-	if len(queue.Songs) != 0 && queue.Lock {
-		lastSong := queue.Songs[len(queue.Songs)-1]
-		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Got it! %s has been added to the queue, and his position is %d", lastSong.Title, lastSong.Position))
-		return
-	}
-
-	queue.Lock = true
 	song := queue.shift()
+	song.stream(s, m)
 
-	for _, guild := range s.State.Guilds {
-		for _, voiceState := range guild.VoiceStates {
-			// TODO: check if the user is in the same voice channel as the bot
-
-			if voiceState.UserID == m.Author.ID {
-				if voiceState.UserID != s.State.User.ID {
-					s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Yay! Joining the party on <#%s>", voiceState.ChannelID))
-				}
-
-				vc, err := s.ChannelVoiceJoin(guild.ID, voiceState.ChannelID, false, true)
-				if err != nil {
-					s.ChannelMessageSend(m.ChannelID, "Oops! It seems that I'm not in a mood for singing right now. Please try again later.")
-					fmt.Println("[exception] Voice connection error:", err)
-					return
-				}
-
-				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Now playing %s", song.Title))
-
-				dir, err := os.Getwd()
-				if err != nil {
-					fmt.Println("[exception] Get working directory error:", err)
-					return
-				}
-
-				song.streamNow(vc, dir+"/songs/song.mp3")
-			}
-		}
-	}
-
-	queue.Lock = false
+	go queue.process(s, m)
 }
 
 func playSongCommand(bot *Bot, s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
-	if len(m.Content) == 0 {
-		s.ChannelMessageSendReply(m.ChannelID, "Guess you forgot to provide a song name", m.Reference())
+	// TODO: check if the host pc has required dependencies installed - ffmpeg
+
+	if len(args) == 0 {
+		s.ChannelMessageSendReply(m.ChannelID, "Guess you forgot to provide a song url", m.Reference())
 		return
 	}
 
-	// TODO: search for a song on youtube and handle the response properly
+	songUrl := args[0]
 
-	song := &Song{
-		Title:       m.Content,
-		RequestedBy: m.Author.ID,
-		Position:    len(bot.Queue.Songs) + 1,
-		Url:         "",
-		StreamUrl:   "",
-		ArtworkUrl:  "",
-		Duration:    "",
+	v, err := bot.Features.Integrations.Youtube.GetVideo(songUrl)
+	if err != nil {
+		s.ChannelMessageSendReply(m.ChannelID, "Oops! It seems something went wrong while searching for your song. Please try again later", m.Reference())
+		log.Println("[exception] Youtube video request error:", err)
+		return
 	}
 
-	bot.Queue.add(*song)
-	bot.Queue.process(s, m)
+	av := v.Formats.WithAudioChannels()[0]
+	rs, _, err := bot.Features.Integrations.Youtube.GetStream(v, &av)
+	if err != nil {
+		s.ChannelMessageSendReply(m.ChannelID, "Oops! It seems something went wrong while searching for your song. Please try again later", m.Reference())
+		log.Println("[exception] Youtube stream request error:", err)
+		return
+	}
+
+	song := bot.Features.Queue.add(&Song{
+		Title:       v.Title,
+		Url:         songUrl,
+		ArtworkUrl:  v.Thumbnails[0].URL,
+		Duration:    v.Duration.Minutes(),
+		RequestedBy: m.Author.ID,
+		Position:    len(bot.Features.Queue.Songs) + 1,
+		Stream: &Stream{
+			Url:          av.URL,
+			MimeType:     av.MimeType,
+			AudioQuality: av.AudioQuality,
+			Bitrate:      av.Bitrate,
+			Readable:     rs,
+		},
+	})
+
+	if bot.Features.Queue.Status == Playing {
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Got it! [%s](%s) has been queued at position %d", song.Title, song.Url, song.Position))
+	}
+
+	bot.Features.Queue.process(s, m)
 }
 
 func pingCommand(bot *Bot, s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
@@ -161,33 +296,21 @@ func pingCommand(bot *Bot, s *discordgo.Session, m *discordgo.MessageCreate, arg
 }
 
 func (bot *Bot) onMessageInteractionCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.Bot || !strings.HasPrefix(m.Content, bot.CommandPrefix) {
+	if m.Author.Bot || !strings.HasPrefix(m.Content, bot.Command.Prefix) {
 		return
 	}
 
-	maybeCommand := strings.TrimLeft(m.Content, bot.CommandPrefix)
+	maybeCommand := strings.TrimLeft(m.Content, bot.Command.Prefix)
 	if len(maybeCommand) == 0 {
-		s.ChannelMessageSend(m.ChannelID, "Provided an invalid command")
+		s.ChannelMessageSend(m.ChannelID, "Hmm... It seems that you forgot to provide a command")
 		return
-	}
-
-	commands := []Command{
-		{
-			Name:    "ping",
-			Execute: pingCommand,
-		},
-		{
-			Name:    "play",
-			Execute: playSongCommand,
-		},
 	}
 
 	c := strings.Split(maybeCommand, " ")
 	commandRef := c[0]
 	commandArgs := c[1:]
-	m.Content = strings.TrimLeft(maybeCommand, commandRef)
 
-	for _, command := range commands {
+	for _, command := range bot.Command.All {
 		if commandRef == command.Name {
 			command.Execute(bot, s, m, commandArgs)
 			return
@@ -196,19 +319,47 @@ func (bot *Bot) onMessageInteractionCreate(s *discordgo.Session, m *discordgo.Me
 }
 
 func main() {
+	if err := env.Load(); err != nil {
+		panic(fmt.Sprintf("[fatal] Environment variables load error: %s", err))
+	}
+
+	botToken := os.Getenv("BOT_TOKEN")
+	botPrefix := os.Getenv("BOT_PREFIX")
+	if len(botToken) == 0 || len(botPrefix) == 0 {
+		panic("[fatal] Missing required environment variables error")
+	}
+
 	bot := Bot{
-		Token:         "NzY0NzQxODA4NTg0MjYxNjUy.GpGXj0.mCfntxocMWyxpBLHFcgbmqICb9UgSU5o_B7Vkw",
-		CommandPrefix: "+",
-		Session:       nil,
-		Queue: &Queue{
-			Lock:  false,
-			Songs: []Song{},
+		Token:   botToken,
+		Session: nil,
+		Command: &Command{
+			Prefix: botPrefix,
+			All: []CallableCommand{
+				{
+					Name:    "ping",
+					Execute: pingCommand,
+				},
+				{
+					Name:    "play",
+					Execute: playSongCommand,
+				},
+			},
+		},
+		Features: &Features{
+			Queue: &Queue{
+				Status: Idle,
+				Mutex:  &sync.Mutex{},
+				Songs:  []Song{},
+			},
+			Integrations: &Integrations{
+				Youtube: &youtube.Client{},
+			},
 		},
 	}
 
 	ds, err := discordgo.New("Bot " + bot.Token)
 	if err != nil {
-		panic(fmt.Sprintf("[fatal] Discord session error: %s", err))
+		panic(fmt.Sprintf("[fatal] Discord session creation error: %s", err))
 	}
 
 	bot.Session = ds
@@ -216,17 +367,18 @@ func main() {
 
 	err = bot.Session.Open()
 	if err != nil {
-		panic(fmt.Sprintf("[fatal] Websocket open error: %s", err))
+		panic(fmt.Sprintf("[fatal] Discord websocket open connection error: %s", err))
 	}
 
-	fmt.Println("[info] Bot started and ready to listen interactions")
-	fmt.Println("[info] Press CTRL-C to shutdown the bot and exit")
+	log.Println(strings.Repeat("-", 20))
+	log.Println("[info] Bot is connected and ready to listen for interactions")
+	log.Println("[info] Press CTRL-C to kill the process and exit")
 
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 
 	if err := bot.Session.Close(); err != nil {
-		panic(fmt.Sprintf("[fatal] Websocket close error: %s", err))
+		panic(fmt.Sprintf("[fatal] Discord websocket close connection error: %s", err))
 	}
 }
